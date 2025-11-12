fa25a4ba7ae28f013bd3ce720d08018c099cfa9e
209155548 <lour_s@outlook.com>
2025-11-12 18:30:01 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,7 +1,9 @@
 def  move(my_list, direction=None):
+     # finds the index of the one in the list 
      index_of_one = my_list.index(1)
-
-    if direction == 'right':
+     
+     # move the one to the left or to the right
+     if direction == 'right':
       if index_of_one!=len(my_list)-1:
             my_list[index_of_one] = 0
             my_list[index_of_one + 1] = 1
@@ -10,4 +12,4 @@ def  move(my_list, direction=None):
          my_list[index_of_one] = 0
          my_list[index_of_one - 1] = 1
 
-    return my_list
+ return my_list
